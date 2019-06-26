358c1640de14028a61b57705302acfd7e54281e9
Lake Bramlett and Shawna Duvall <lake+shawna@>
2019-06-26 11:19:12 -0700

    add initial html form and JS logic

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import  './styles.css';
+import $ from 'jquery';
+
+
+$(document).ready(function () {
+const charName = $('#user-input-name').val();
+const charAge = $('#user-input-age').val();
+const charRace = $('#user-input-race').val();
+const charClass = $('#user-input-class').val();
+
+});
